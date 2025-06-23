d5ce6d5e3cc5b646aa990f37f4a4524843bcd247
Joeltom <92246279+johnwik5377@users.noreply.github.com>
2025-06-23 05:52:43 +0000

    Initial commit: Basic Spring Boot project setup with Maven wrapper and default configuration

---
+++ tickbox-task-manager/src/main/java/com/tickbox/tickbox_task_manager/TickboxTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tickbox.tickbox_task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickboxTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickboxTaskManagerApplication.class, args);
+	}
+
+}
---
+++ tickbox-task-manager/src/test/java/com/tickbox/tickbox_task_manager/TickboxTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickbox.tickbox_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickboxTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
